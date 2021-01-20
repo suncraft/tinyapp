37e518495a63d0f150ddbb3c53b049d082ca91e1
Suncraft <cookies666@msn.com>
2021-01-20 14:41:54 +0000

    added delete button

--- express_server.js
+++ express_server.js
@@ -42,6 +42,11 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${newShortURL}`);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
 app.get("/urls", (req, res) => { //added 1st
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
