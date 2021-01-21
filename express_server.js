9f7ca7274928f4fcfa9aaf73e3e0e4a4c195b5cd
Suncraft <cookies666@msn.com>
2021-01-21 22:26:13 +0000

    working register and show email

--- express_server.js
+++ express_server.js
@@ -10,54 +10,47 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-// ###### MINE WASN'T WORKING ############
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
-
-
 let urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "test1" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "test1" }
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
 };
 
-let users = { "test1": "one"};
-
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 //routes
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  let email = getEmailFunct(req.cookies["user_id"]);
+  const templateVars = { user_id: req.cookies["user_id"], email: email };
   res.render("urls_new", templateVars);
 });
 
 //register
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  let email = getEmailFunct(req.cookies["user_id"]);
+  const templateVars = { user_id: req.cookies["user_id"], email: email };
   res.render("register", templateVars);
 });
 
 // login
 app.get("/login", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  let email = getEmailFunct(req.cookies["user_id"]);
+  const templateVars = { user_id: req.cookies["user_id"], email: email };
   res.render("login", templateVars);
 });
 
-
-
-// ###### MINE WASN'T WORKING ############
-// ###### Error: TypeError: Cannot read property 'longURL' of undefined
-// at app.post (/vagrant/w3/tinyapp/express_server.js:21:39) ############
-
-// app.post('/urls', (req, res) => {
-//   // console.log(req.body);  // Log the POST request body to the console
-//   let newShortURL = generateRandomString();
-//   urlDatabase[newShortURL] = res.body.longURL;
-//   res.redirect(`/urls/${newShortURL}`);         // Respond with 'Ok' (we will replace this)
-// });
-
+//new short url
 app.post('/urls', (req, res) => {
     console.log(req.body); 
   let newShortURL = generateRandomString();
@@ -66,80 +59,98 @@ app.post('/urls', (req, res) => {
   };
   res.redirect(`/urls/${newShortURL}`);
 });
-
+//login
 app.post('/login', (req, res) => {
   console.log(req.body);
   res.cookie("username", req.body.username);
   res.redirect('/urls');
 });
 
-//logout
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls')
+// register
+app.post('/register', (req, res) => {
+  // console.log(req.body);
+  const newID = generateRandomString();
+  users[newID] = { id: newID, email: req.body.email, password: req.body.password }
+  console.log(users);
+  res.cookie("user_id", users[newID].id)
+  res.redirect('/urls');
 });
 
+//logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls')
+});
+//delete short url
 app.post('/urls/:shortURL/delete', (req, res) => {
   console.log(`Deleting: ${urlDatabase[req.params.shortURL]}`); //?
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls`);
 });
-
+//edit short url
 app.post('/urls/:shortURL/edit', (req, res) => {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL
   console.log(req.body); 
 
   res.redirect(`/urls`);
 });
-
+//index page
 app.get("/urls", (req, res) => { //added 1st
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  let email = getEmailFunct(req.cookies["user_id"]);
+  const templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"], email: email };
   res.render("urls_index", templateVars);
 });
-
+//when refering to a short url
 app.get("/urls/:shortURL", (req, res) => { // added 2nd
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, username: req.cookies["username"] }; 
+  let email = getEmailFunct(req.cookies["user_id"]); //do I need???
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user_id: req.cookies["user_id"], email: email}; 
   //I have to use params.shortURL to access the name above ^^
   res.render("urls_show", templateVars);
 });
-
+//redirecting to the endpoint long url
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
-// app.post('/memes', (req, res) => {
-//   // hendle adding the meme
-//   // body parser makes the body available in req.body as js object
-//   dataHelpers.addMeme(req.body.topText, req.body.bottomText, req.body.image);
-//   res.redirect('/memes');
-// })
-
 app.get("/", (req, res) => {
   res.send("Hello");
 })
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-//  //TESTING CODE: a = 1 is block scoped, won't see it in,
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
-//  //THIS ONE: a is undefined.
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
 
 function generateRandomString() {
 return Math.random().toString(36).substr(2, 6); //.substr(2, length) 
 };
+
+// let idByEmail = (users, email) => {
+//   for (let user in users) {
+//     console.log(user);
+//     if (users[user].email === email) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+const getEmailFunct = function (userid) {
+  for (let user in users) {
+    if (users[user].id === userid) {
+      return users[user].email;
+    }
+  }
+};
+
+
+// //future register from lecture
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(plaintextpassword, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// })
+//comparing
+// bcrypt.compare('12345plaintextpass', hashed, (err, result) => { //true or false
+//   console.log(result);
+// });
