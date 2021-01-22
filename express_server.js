98ac7a8092a91fc1b69bdd170e23e6a55a4906b3
Suncraft <cookies666@msn.com>
2021-01-22 23:25:03 +0000

    linting and JSON editing

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser");
 const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 
 const { generateRandomString, idByEmail, getEmailFunct } = require("./helpers.js");
 
@@ -20,22 +20,22 @@ app.use(cookieSession({
 
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
 
 let urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "$2b$10$YzA9gKgZovqwHlImFQLK5uzdkCS30Nq64GZfn3fStuRewv4MUzMd6"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "$2b$10$/WruIgXKlREbuTM0QmDzP.EgHPGDls9qhEAtCeTOz.WXUO.KXk3xW"
   }
   //purple, and dishwasher, are the passwords for testing
@@ -69,7 +69,7 @@ app.get("/login", (req, res) => {
 
 //new short url
 app.post('/urls', (req, res) => {
-    console.log(req.body); 
+  console.log(req.body);
   let newShortURL = generateRandomString();
   urlDatabase[newShortURL] = {
     longURL: req.body.longURL,
@@ -90,18 +90,18 @@ app.post('/login', (req, res) => {
   }
   for (let user in users) {
     if (users[user].email === req.body.email) {
-      if (bcrypt.compareSync(req.body.password, users[user].password)) { 
+      if (bcrypt.compareSync(req.body.password, users[user].password)) {
         req.session.user_id = users[user].id;
         return res.redirect('/urls');
       }
-      }
     }
-    return res.send('403 Error from last res')
-  });
+  }
+  return res.send('403 Error from last res');
+});
 
-  // setTimeout(res.redirect('/login'), 5000);
+// setTimeout(res.redirect('/login'), 5000);
 
-// if (users[user].password === req.body.password) 
+// if (users[user].password === req.body.password)
 
 // //future register from lecture
 // bcrypt.genSalt(10, (err, salt) => {
@@ -128,14 +128,14 @@ app.post('/register', (req, res) => {
     return res.send('400 Error');
   }
   if (idByEmail(req.body.email, urlDatabase) === true) {
-    return res.send('400 Error')
+    return res.send('400 Error');
   }
   const newID = generateRandomString();
   const newHashedPass = bcrypt.hashSync(req.body.password, 10);
 
-  users[newID] = { id: newID, email: req.body.email, password: newHashedPass }
+  users[newID] = { id: newID, email: req.body.email, password: newHashedPass };
   console.log(users);
-  req.session.user_id = users[newID].id;  //* new *
+  req.session.user_id = users[newID].id;
   res.redirect('/urls');
 });
 
@@ -143,12 +143,12 @@ app.post('/register', (req, res) => {
 app.post('/logout', (req, res) => {
   req.session = null; //new
   // res.clearCookie('user_id');
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 //delete short url
 app.post('/urls/:shortURL/delete', (req, res) => {
   if (!req.session.user_id) {
-    return res.redirect('/login')
+    return res.redirect('/login');
   }
   console.log(`Deleting: ${urlDatabase[req.params.shortURL]}`);
   delete urlDatabase[req.params.shortURL];
@@ -159,8 +159,8 @@ app.post('/urls/:shortURL/edit', (req, res) => {
   if (!req.session.user_id) {
     return res.redirect('/urls');
   }
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL
-  console.log(req.body); 
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  console.log(req.body);
 
   res.redirect(`/urls`);
 });
@@ -169,10 +169,10 @@ app.post('/urls/:shortURL/edit', (req, res) => {
 app.get("/urls", (req, res) => { //added 1st
   let tempObj = {};
   for (let singleURL in urlDatabase) {
-      if (urlDatabase[singleURL]["userID"] === req.session.user_id) {
-      tempObj[singleURL] = urlDatabase[singleURL]; 
+    if (urlDatabase[singleURL]["userID"] === req.session.user_id) {
+      tempObj[singleURL] = urlDatabase[singleURL];
     }
-  }; 
+  }
   // console.log(tempObj);
   let urls = tempObj;
   let email = getEmailFunct(req.session.user_id, users);
@@ -186,13 +186,13 @@ app.get("/urls/:shortURL", (req, res) => { // added 2nd
     return res.redirect('/urls');
   }
   let email = getEmailFunct(req.session.user_id, users);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user_id: req.session.user_id, email: email}; 
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user_id: req.session.user_id, email: email};
   //I have to use params.shortURL to access the name above ^^
   res.render("urls_show", templateVars);
 });
 //redirecting to the endpoint long url
 app.get("/u/:shortURL", (req, res) => {
-  if (req.params.shortURL != urlDatabase[req.params.shortURL]) {
+  if (req.params.shortURL !== urlDatabase[req.params.shortURL]) {
     return res.redirect('/urls');
   }
   const longURL = urlDatabase[req.params.shortURL].longURL;
@@ -201,10 +201,10 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/", (req, res) => {
   if (!req.session.user_id) {
-    return res.redirect('/login')
+    return res.redirect('/login');
   }
   res.redirect("urls");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
